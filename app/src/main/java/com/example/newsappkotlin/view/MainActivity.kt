950cdbf665092bb287be85d5cf80cbf46d8a7d25
kadriyeg <denizk2407@gmail.com>
2022-10-08 22:09:46 +0300

    Fragments, fragmentsler için navigation ve arguments eklendi

--- app/src/main/java/com/example/newsappkotlin/view/MainActivity.kt
+++ app/src/main/java/com/example/newsappkotlin/view/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.example.newsappkotlin
+package com.example.newsappkotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsappkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
